f8cf095e23307c27e1cd9ffed98b7063125bf6b5
hssung-0011 <hsung@DESKTOP-0DCED8Q.>
2025-03-31 18:07:42 +0900

    3/31

--- PROT.py
+++ PROT.py
@@ -23,29 +23,6 @@ rna_to_prot = {}
 for num in range(0,len(trans),2):
     rna_to_prot[trans[num]] = trans[num+1]
 
-def change(seq):
-    trans = """UUU F      CUU L      AUU I      GUU V
-    UUC F      CUC L      AUC I      GUC V
-    UUA L      CUA L      AUA I      GUA V
-    UUG L      CUG L      AUG M      GUG V
-    UCU S      CCU P      ACU T      GCU A
-    UCC S      CCC P      ACC T      GCC A
-    UCA S      CCA P      ACA T      GCA A
-    UCG S      CCG P      ACG T      GCG A
-    UAU Y      CAU H      AAU N      GAU D
-    UAC Y      CAC H      AAC N      GAC D
-    UAA Stop   CAA Q      AAA K      GAA E
-    UAG Stop   CAG Q      AAG K      GAG E
-    UGU C      CGU R      AGU S      GGU G
-    UGC C      CGC R      AGC S      GGC G
-    UGA Stop   CGA R      AGA R      GGA G
-    UGG W      CGG R      AGG R      GGG G""".split()
-    rna_to_prot = {}
-
-    for num in range(0,len(trans),2):
-        rna_to_prot[trans[num]] = trans[num+1]
-    return rna_to_prot[seq]
-
 
 ## sequence를 세 단위씩 읽어서 변환환
 for i in range(0,len(rna_seq),3):
